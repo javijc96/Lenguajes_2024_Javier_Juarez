1c3119c25ca6771a7f672f34f8ecdcf80123b8a8
javier juarez canals <jjc0013@alu.medac.es>
2025-05-06 16:35:02 +0200

    vacaciones mejorado con js

---
+++ tercertrimestre/tema18-19/contenido/ejerciciosEventos/js/vacaciones.js
@@ -0,0 +1,29 @@
+const curro = document.getElementById("curro");
+const mensaje = document.getElementById("mensaje");
+const destinos = document.querySelectorAll(".destino");
+
+// Evento: cuando comienza a arrastrarse
+curro.addEventListener("dragstart", (e) => {
+    e.dataTransfer.setData("text/plain", "curro");
+});
+
+// Cada destino acepta a curro
+destinos.forEach(destino => {
+    destino.addEventListener("dragover", (e) => {
+        e.preventDefault(); // Necesario para que pueda soltarse
+    });
+
+    destino.addEventListener("drop", (e) => {
+        e.preventDefault();
+
+        const destinoNombre = destino.getAttribute("data-destino");
+
+        // Mover a curro al centro del destino
+        destino.appendChild(curro);
+
+        // Actualizar mensaje
+        mensaje.textContent = `Vas a ${destinoNombre}`;
+
+
+    });
+});
