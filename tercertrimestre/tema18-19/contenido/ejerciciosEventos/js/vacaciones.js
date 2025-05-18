d7b8144d7735f035efa85d3b1bc4a62cf545c15d
javijc96 <jjc0013@alu.medac.es>
2025-05-18 13:18:53 +0200

    vacaciones ya

--- tercertrimestre/tema18-19/contenido/ejerciciosEventos/js/vacaciones.js
+++ tercertrimestre/tema18-19/contenido/ejerciciosEventos/js/vacaciones.js
@@ -1,6 +1,9 @@
 const curro = document.getElementById("curro");
 const mensaje = document.getElementById("mensaje");
 const destinos = document.querySelectorAll(".destino");
+const volver = document.getElementById("volver");
+const zonacurro = document.getElementById("zona-curro");
+
 
 // Evento: cuando comienza a arrastrarse
 curro.addEventListener("dragstart", (e) => {
@@ -16,12 +19,32 @@ destinos.forEach(destino => {
     destino.addEventListener("drop", (e) => {
         e.preventDefault();
 
-        const destinoNombre = destino.getAttribute("data-destino");
 
         // Mover a curro al centro del destino
         destino.appendChild(curro);
+        curro.style.position = "absolute";
+        curro.style.top = "50%";
+        curro.style.left = "50%";
+        curro.style.transform = "translate(-50%, -50%)";
 
         // Actualizar mensaje
+        const destinoNombre = destino.dataset.destino;
+
         mensaje.textContent = `Vas a ${destinoNombre}`;
     });
+});
+
+// Al hacer clic en el botón "Volver al origen"
+volver.addEventListener("click", () => {
+    // Colocamos el curro de nuevo dentro de su contenedor original
+    zonacurro.appendChild(curro);
+
+    // Restauramos la posición original (sin absolute)
+    curro.style.position = "relative";
+    curro.style.top = "0";
+    curro.style.left = "0";
+    curro.style.transform = "none";
+
+    // Cambiamos el mensaje a su estado inicial
+    mensaje.textContent = "Me voy de vacaciones...";
 });
